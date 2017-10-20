5a76a2a9b05e92422da04b1853f5e27b1ca830e4
Louwii <link14.online@gmail.com>
2017-10-20 16:54:45 -0700

    add logic for previous and next action and buttons in Player

--- app/components/Player.js
+++ app/components/Player.js
@@ -7,6 +7,8 @@ class Player extends React.Component {
     super(props)
 
     this.handlePlayPauseClick = this.handlePlayPauseClick.bind(this)
+    this.handleNextClick = this.handleNextClick.bind(this)
+    this.handlePrevClick = this.handlePrevClick.bind(this)
 
     const songUrl = ''
     const playing = false
@@ -40,11 +42,21 @@ class Player extends React.Component {
     // console.log(audio.currentSrc, audio.currentTime, audio.duration)
   }
 
+  handlePrevClick() {
+    this.props.playerPrevious()
+  }
+
+  handleNextClick() {
+    this.props.playerNext()
+  }
+
   render() {
     const playPauseActionText = (this.props.player.status==='play'?'Pause':'Play')
     return (
       <div className="player">
+        <button onClick={this.handlePrevClick} type="button">Prev</button>
         <button onClick={this.handlePlayPauseClick} type="button">{playPauseActionText}</button>
+        <button onClick={this.handleNextClick} type="button">Next</button>
         <p>{this.props.player.songs.length} loaded ({this.props.player.currentPlaylistId}) - Song {this.props.player.currentSongIdx} - Status {this.props.player.status}</p>
         { (this.props.player.currentSongIdx || this.props.player.currentSongIdx === 0) &&
         <audio id="audio" src={this.state.songUrl} ></audio>
