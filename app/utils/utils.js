a3341c5ed6ae03ea75de630c4b07f450e12e0854
Louwii <link14.online@gmail.com>
2017-10-21 16:53:07 -0700

    remove old code from utils and add method to convert duration

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -28,16 +28,18 @@ export function capitalize(string = ''){
   ).join('')
 }
 
-export const statusToIcon = {
-  'waiting': 'glyphicon glyphicon-hourglass',
-  'downloading': 'glyphicon glyphicon-save',
-  'paused': 'glyphicon glyphicon-pause',
-  'finishing': 'glyphicon glyphicon-refresh',
-  'finished': 'glyphicon glyphicon-ok',
-  'hash_checking': 'glyphicon glyphicon-tasks',
-  'seeding': 'glyphicon glyphicon-export',
-  'filehosting_waiting': 'glyphicon glyphicon-cloud',
-  'extracting': 'glyphicon glyphicon-compressed',
-  'error': 'glyphicon glyphicon-alert',
-  'other': 'glyphicon glyphicon-question-sign'
+/**
+ * Convert a duration in seconds to a human readable duration
+ * @param {integer} duration 
+ */
+export function getHumanDuration(duration) {
+  var sec_num = parseInt(duration, 10); // don't forget the second param
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return (hours!=='00'?hours+':':'')+minutes+':'+seconds;
 }
