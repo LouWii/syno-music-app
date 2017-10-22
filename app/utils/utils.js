5913a5b2bf26caecb618f1e65ccc0520918c5ed6
Louis Dekeister <louis@iMac-de-Louis.local>
2017-10-22 00:57:36 -0700

    update utils method to not display front 0 in times

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -39,7 +39,7 @@ export function getHumanDuration(duration) {
   var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
   if (hours   < 10) {hours   = "0"+hours;}
-  if (minutes < 10) {minutes = "0"+minutes;}
-  if (seconds < 10) {seconds = "0"+seconds;}
+  if (hours!=='00' && minutes < 10) {minutes = "0"+minutes;}
+  if (minutes!=='00' && seconds < 10) {seconds = "0"+seconds;}
   return (hours!=='00'?hours+':':'')+minutes+':'+seconds;
 }
