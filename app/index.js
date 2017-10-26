6ee5aeb2da482432d79acbcf7824e09a9439ac58
Louis Dekeister <louis@iMac-de-Louis.local>
2017-10-25 18:15:20 -0700

    replace app css with base scss and removed old rules

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
-import './styles/app.global.css';
+import './styles/base.global.scss';
 
 const store = configureStore();
 
