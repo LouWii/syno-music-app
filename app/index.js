edcce4d2f8869bb086d6614fa8c87550875769b8
Louis Dekeister <louis@iMac-de-Louis.local>
2017-10-25 17:27:08 -0700

    update index and add Root component for base structure of the app

--- app/index.js
+++ app/index.js
@@ -1,32 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, IndexRoute, Route, hashHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import configureStore, { saveState } from './store/configureStore';
-import App from './components/App';
-import ProfilesList from './components/ProfilesList'
-import ProfileAdd from './components/ProfileAdd'
-import ASClient from './components/ASClient'
-
+import { AppContainer } from 'react-hot-loader';
+import Root from './containers/Root';
+import { configureStore, history } from './store/configureStore';
 import './styles/app.global.css';
 
 const store = configureStore();
-const history = syncHistoryWithStore(hashHistory, store);
-
-store.subscribe(() => {
-  saveState(store.getState())
-});
 
 render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={ProfilesList}></IndexRoute>
-        <Route path="/profiles/add" component={ProfileAdd}></Route>
-        <Route path="/profiles/:idx" component={ASClient}></Route>
-      </Route>
-    </Router>
-  </Provider>,
+  <AppContainer>
+    <Root store={store} history={history} />
+  </AppContainer>,
   document.getElementById('root')
 );
+
+if (module.hot) {
+  module.hot.accept('./containers/Root', () => {
+    const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
+    render(
+      <AppContainer>
+        <NextRoot store={store} history={history} />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
