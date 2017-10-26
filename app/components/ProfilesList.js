ad6866b3cdd76f4f844567bfa8f2754cd818188d
Louis Dekeister <louis@iMac-de-Louis.local>
2017-10-25 18:19:57 -0700

    use scss for ProfilesList

--- app/components/ProfilesList.js
+++ app/components/ProfilesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import Profile from './Profile'
-import '../styles/ProfilesList.global.css';
+import '../styles/ProfilesList.global.scss';
 
 export default class ProfilesList extends React.Component {
 
